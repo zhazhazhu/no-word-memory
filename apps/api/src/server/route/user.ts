8e461a7789a3323d9de105233b776b087e690191
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-05-21 16:08:11 +0800

    fix: correct schema reference for user retrieval and update table export

--- apps/api/src/server/route/user.ts
+++ apps/api/src/server/route/user.ts
@@ -3,7 +3,7 @@ import { publicProcedure, router } from '../trpc';
 
 const list = publicProcedure.query(async () => {
   // Retrieve users from a datasource, this is an imaginary database
-  const users = await db.select().from(schemas.usersTable).all();
+  const users = await db.select().from(schemas.users).all();
 
   return users;
 });
--- packages/schema/src/schema/user.ts
+++ packages/schema/src/schema/user.ts
@@ -1,8 +1,8 @@
 import { sql } from 'drizzle-orm';
-import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const usersTable = sqliteTable('users_table', {
-  id: int().primaryKey({ autoIncrement: true }),
+export const users = sqliteTable('users_table', {
+  id: text().primaryKey().$defaultFn(() => sql`ROWID`),
   name: text().notNull(),
   email: text().notNull(),
   createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`),
