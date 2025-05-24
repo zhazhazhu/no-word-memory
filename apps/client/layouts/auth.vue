dc9a7f8e58ff26597049691362d2aa40e4259bcf
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-05-24 17:52:16 +0800

    fix: correct positioning of background elements in authentication layout

--- apps/client/layouts/auth.vue
+++ apps/client/layouts/auth.vue
@@ -6,7 +6,7 @@
   <div class="flex items-center justify-center h-screen relative prose">
     <div class="absolute top-0 -z-10 h-full w-full bg-white">
       <div class="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
-      <div class="absolute bottom-auto left-auto left-0 top-0 h-[500px] w-[500px] -translate-x-[20%] translate-y-[90%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
+      <div class="absolute bottom-auto left-0 top-0 h-[500px] w-[500px] -translate-x-[20%] translate-y-[90%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
     </div>
 
     <div class="size-[600px] bg-white rounded-3xl shadow-xl">
--- apps/client/pages/auth/verify-request.vue
+++ apps/client/pages/auth/verify-request.vue
@@ -1,5 +1,6 @@
 <script lang='ts' setup>
 definePageMeta({
+  auth: { unauthenticatedOnly: true },
   layout: 'auth',
 });
 </script>
